ae049f7c4549cb97028a0fddc06d74a03fdf574f
abdulrahmansbq <abdulrahmansbq@gmail.com>
2023-09-01 18:55:13 +0300

    Make the nlp engine as an abstract class

--- modules/engines/nlp_engine.py
+++ modules/engines/nlp_engine.py
@@ -1,4 +1,7 @@
-class NlpEngine:
+import abc
+
+
+class NlpEngine(abc.ABC):
     def __init__(self, vulnerability):
         self.prompts = {
             "vulnerability_description": f"describe the following bug {vulnerability} with details to technical and non technical people",
@@ -6,17 +9,7 @@ class NlpEngine:
             "suggestions": f"what do you suggest to fix this {vulnerability} write full details"
         }
 
-    def __get_vulnerability_description(self):
-        pass
 
-    def __get_impact_description(self):
-        pass
-
-    def __get_suggestions(self):
-        pass
-
+    @abc.abstractmethod
     def get_contents(self):
-        vulnerability_desc = self.__get_vulnerability_description()
-        impact_desc = self.__get_impact_description()
-        suggestions = self.__get_suggestions()
-        return vulnerability_desc, impact_desc, suggestions
+        pass
