ab9dbb96dc6a5cd8b8e347062573449fbea52f68
abdulrahmansbq <abdulrahmansbq@gmail.com>
2023-09-01 18:54:50 +0300

    Remove the prompts from the chagpt engine to be only in the parent class

--- modules/engines/openai_chatgpt.py
+++ modules/engines/openai_chatgpt.py
@@ -1,29 +1,29 @@
 import openai
 from modules.engines.nlp_engine import NlpEngine
-import os
 
 
 class OpenaiChatgpt(NlpEngine):
 
-    def __init__(self, vulnerability, api_key, max_tokens=2500, engine="text-davinci-002"):
+    def __init__(self, vulnerability, api_key, max_tokens=2500, model="text-davinci-002"):
         super().__init__(vulnerability)
-        self.prompts = {
-            "vulnerability_description": f"describe the following bug {vulnerability} with details to technical and non technical people",
-            "impact_description": f"what is the impact of this {vulnerability} and how it might affect the company",
-            "suggestions": f"what do you suggest to fix this {vulnerability} write full details"
-        }
         openai.api_key = api_key
         self.max_tokens = max_tokens
-        self.engine = engine
+        self.model = model
 
     def __get_vulnerability_description(self):
-        response = openai.Completion.create(model=self.engine, prompt=self.prompts['vulnerability_description'], max_tokens=self.max_tokens)
+        response = openai.Completion.create(model=self.model, prompt=self.prompts['vulnerability_description'], max_tokens=self.max_tokens)
         return response.choices[0].text.strip()
 
     def __get_impact_description(self):
-        response = openai.Completion.create(model=self.engine, prompt=self.prompts['impact_description'], max_tokens=self.max_tokens)
+        response = openai.Completion.create(model=self.model, prompt=self.prompts['impact_description'], max_tokens=self.max_tokens)
         return response.choices[0].text.strip()
 
     def __get_suggestions(self):
-        response = openai.Completion.create(model=self.engine, prompt=self.prompts['suggestions'], max_tokens=self.max_tokens)
+        response = openai.Completion.create(model=self.model, prompt=self.prompts['suggestions'], max_tokens=self.max_tokens)
         return response.choices[0].text.strip()
+
+    def get_contents(self):
+        vulnerability_desc = self.__get_vulnerability_description()
+        impact_desc = self.__get_impact_description()
+        suggestions = self.__get_suggestions()
+        return vulnerability_desc, impact_desc, suggestions
