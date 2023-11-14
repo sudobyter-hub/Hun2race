ee629267471b6a8cc2e50fd8b8c58806134f7e3d
Ali Waleed <82229642+sudobyter-hub@users.noreply.github.com>
2023-11-14 23:08:34 +0300

    its working prefectly

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
 import os
 import subprocess
 from datetime import datetime
-from flask import (
-    Flask,
-    render_template,
-    request,
-    redirect,
-    url_for,
-    send_from_directory,
-    flash,
-)
+from flask import Flask, render_template, request, send_from_directory
 from werkzeug.utils import secure_filename
 import openai
 from dotenv import load_dotenv
@@ -20,9 +12,7 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = "uploads"
-app.config["SECRET_KEY"] = os.getenv(
-    "SECRET_KEY", "a-random-secret-key"
-)  # Use a secure secret key
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "a-random-secret-key")
 
 # Ensure the upload folder exists
 os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
@@ -30,22 +20,13 @@ os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 # Initialize OpenAI API
 openai.api_key = OPENAI_API_KEY
 
-
 def allowed_file(filename):
-    return "." in filename and filename.rsplit(".", 1)[1].lower() in {
-        "png",
-        "jpg",
-        "jpeg",
-        "gif",
-        "bmp",
-        "pdf",
-    }
-
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in {"png", "jpg", "jpeg", "gif", "bmp", "pdf"}
 
 def query_openai(prompt):
     try:
         response = openai.Completion.create(
-            engine="text-davinci-003",  # Ensure this is the correct engine name
+            engine="text-davinci-003",
             prompt=prompt,
             max_tokens=150,
         )
@@ -54,11 +35,7 @@ def query_openai(prompt):
         print(f"An error occurred: {e}")
     return "An error occurred while querying the AI."
 
-
-def generate_latex_document(
-    vuln_name, vuln_desc, poc, vuln_impact, vuln_mitigation, saved_files
-):
-    # LaTeX document header
+def generate_latex_document(vuln_name, hostname, vuln_desc, poc, vuln_impact, vuln_mitigation, saved_files):
     latex_content = r"""\documentclass{article}
 \usepackage[utf8]{inputenc}
 \usepackage{graphicx}
@@ -67,13 +44,9 @@ def generate_latex_document(
 \date{%s}
 \maketitle
 \newpage
-""" % (
-        vuln_name,
-        datetime.now().strftime("%Y-%m-%d"),
-    )
-
-    # LaTeX document body
-    latex_content += r"""\section*{Vulnerability Description}
+\section*{Hostname/FQDN}
+%s
+\section*{Vulnerability Description}
 %s
 \section*{Proof of Concept}
 %s
@@ -84,32 +57,30 @@ def generate_latex_document(
 \newpage
 \section*{Attachments}
 """ % (
+        vuln_name,
+        datetime.now().strftime("%Y-%m-%d"),
+        hostname,  # Include hostname here
         vuln_desc,
         poc,
         vuln_impact,
         vuln_mitigation,
     )
 
-    # Adding images to the document
     for image_path in saved_files:
-        latex_content += (
-            r"""\begin{figure}[h!]
+        latex_content += r"""\begin{figure}[h!]
 \centering
 \includegraphics[width=0.5\textwidth]{%s}
 \end{figure}
-"""
-            % image_path
-        )
+""" % image_path
 
-    # LaTeX document end
     latex_content += r"\end{document}"
     return latex_content
-
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         vuln_name = request.form["vuln_name"]
+        hostname = request.form["hostname"]  
         poc = request.form["poc"]
         files = request.files.getlist("media")
 
@@ -121,12 +92,14 @@ def index():
                 file.save(filepath)
                 saved_files.append(filepath)
 
-        vuln_desc = query_openai(f"Describe vulnerability {vuln_name}.")
+        # Updated to use specific prompts based on user input
+        vuln_desc_prompt = f"Describe vulnerability {vuln_name} for both technical and non-technical people."
+        vuln_desc = query_openai(vuln_desc_prompt)
         vuln_impact = query_openai(f"What is the impact of {vuln_name}?")
         vuln_mitigation = query_openai(f"How to mitigate {vuln_name}?")
 
         latex_document = generate_latex_document(
-            vuln_name, vuln_desc, poc, vuln_impact, vuln_mitigation, saved_files
+            vuln_name, hostname, vuln_desc, poc, vuln_impact, vuln_mitigation, saved_files
         )
 
         # Save the LaTeX document
@@ -148,7 +121,6 @@ def index():
         )
 
     return render_template("index.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
